39446fff06d0d22063d1ecfa2e66e87898acb560
Ayami Miyazaki <312044099+Ayami31@users.noreply.github.com>
2026-09-21 18:42:08 +0900

    Initialy functions made.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,12 @@
+//ask user for input
+function getHumanChoice() {
+    let userInput = prompt("What is your bet? Rock, Paper, or Scissors? ");
+    return userInput;
+}
+
+console.log("You chose the " + getHumanChoice());
+
+//generate a random choice
 function getComputerChoice() {
     const number = Math.random();
 
@@ -11,7 +20,40 @@ function getComputerChoice() {
     }
     else {
         return "scissors";
-    }
+    }    
 }
 
-console.log(getComputerChoice());
+console.log("The computer chose the " + getComputerChoice());
+
+//the algorithm that will calculate the options given
+function playRound(computer, human) {
+    
+
+    const computer = getComputerChoice().toLocaleLowerCase();
+    const human = getHumanChoice().toLocaleLowerCase();
+
+    if (computer === human) {
+        return console.log("It's a draw!");
+    }
+
+    else if (computer === "rock" && human === "paper") {
+        return 
+
+    }
+
+}
+
+
+
+playRound (computerChoice, humanChoice)
+
+let computerScore = 0;
+let humanScore = 0;
+
+/*
+How many possible situations are there?
+rock beats scissors, scissors beat paper, paper beats rock
+- assign values and calculate?
+putr different if -else values?
+
+*/

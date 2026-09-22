c4f3fa4d41532c209c9f83eca7758526d935dff2
Ayami Miyazaki <312044099+Ayami31@users.noreply.github.com>
2026-09-22 10:05:14 +0900

    Finalize the code.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,18 @@
+//put this outside so the score doenst loop to one or two
+let computerScore = 0;
+let humanScore = 0;
+
+while (true) {
+
+//how do I make this infinite?
+
 //ask user for input
 function getHumanChoice() {
     let userInput = prompt("What is your bet? Rock, Paper, or Scissors? ");
     return userInput;
 }
-
-console.log("You chose the " + getHumanChoice());
+const humanChoice = getHumanChoice().toLocaleLowerCase();
+console.log("You chose the " + humanChoice); 
 
 //generate a random choice
 function getComputerChoice() {
@@ -23,37 +31,58 @@ function getComputerChoice() {
     }    
 }
 
-console.log("The computer chose the " + getComputerChoice());
+const computerChoice = getComputerChoice().toLocaleLowerCase();
+console.log("The computer chose the " + computerChoice);
+
 
 //the algorithm that will calculate the options given
 function playRound(computer, human) {
-    
 
-    const computer = getComputerChoice().toLocaleLowerCase();
-    const human = getHumanChoice().toLocaleLowerCase();
+    console.log("Comparing:", computer, human, "| Scores before:", computerScore, humanScore);
 
     if (computer === human) {
         return console.log("It's a draw!");
     }
-
     else if (computer === "rock" && human === "paper") {
-        return 
-
+        humanScore++;
+        return console.log("Paper beats rock. You win!");
     }
-
+    else if (computer === "paper" && human === "rock") {
+        computerScore++;
+        return console.log("Paper beats rock. Computer wins!");
+    }
+    else if (computer === "scissors" && human === "paper") {
+        computerScore++;
+        return console.log("Scissors beats paper. Computer wins!");
+    }
+    else if (computer === "paper" && human === "scissors") {
+        humanScore++;
+        return console.log("Scissors beats paper. You win!");
+    }
+    else if (computer === "rock" && human === "scissors") {
+        computerScore++;
+        return console.log("Rock beats scissors. Computer wins!");
+    }
+    else if (computer === "scissors" && human === "rock") {
+        humanScore++;
+        return console.log("Rock beats scissors. You win!");
+    }
 }
-
 
 
 playRound (computerChoice, humanChoice)
 
-let computerScore = 0;
-let humanScore = 0;
+console.log("Computer score: " + computerScore + "    " +  "Human Score: " + humanScore);
 
+const playAgain = prompt("Do you want to play again? y for yes, n for no")
+if (playAgain.toLocaleLowerCase() === "n") {
+    break;
+}
 /*
 How many possible situations are there?
 rock beats scissors, scissors beat paper, paper beats rock
 - assign values and calculate?
-putr different if -else values?
-
+put different if -else values?
 */
+
+}
